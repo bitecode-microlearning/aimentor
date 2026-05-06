2ccc45142fa90b10755d7d3c95f1aeb91c887ed1
cserenyeczt <cserenyeczt@gmail.com>
2026-05-06 20:43:03 +0200

    Refactor MentorPanel to manage conversation state and remove ConversationProvider from main.tsx

--- AImentorApp/src/main.tsx
+++ AImentorApp/src/main.tsx
@@ -1,12 +1,7 @@
 
   import { createRoot } from "react-dom/client";
-import { ConversationProvider } from "@elevenlabs/react";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <ConversationProvider>
-    <App />
-  </ConversationProvider>
-);
+createRoot(document.getElementById("root")!).render(<App />);
   
